3724464504d002eacf7f323fd1c795ff80a659c9
Juan <60558831+Koruption@users.noreply.github.com>
2022-02-07 21:46:45 -0600

    Adding dependencies list in requirements.txt and example in ReadMe
    Adding dependencies list in requirements.txt and example in ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # CSV Dataset Generator
  Simple python program for quickly generating datasets and writing them to a csv
+
+
+## Usage
+``` python 
+
+table = DataTable([
+    Column('name', NameGenerator(full_name=False)),
+    Column('email', EmailGenerator()),
+    Column('subscription_status', CategoryGenerator(['subscribed', 'unsubscribed', 'unknown'])),
+    Column('amount_spent', CurrencyGenerator('$', amount_range=(0,250000))),
+    Column('date', DateGenerator('1/1/2022 1:30 pm', '1/22/2022 1:30 pm')),
+])
+
+# define number of rows in table
+table.set_rows(5)
+
+# create row data 
+table.generate_rows()
+
+# display table data 
+table.print_rows()
+
+'''
+outputs: 
+['Johnny', 'joyfulIcecream8@icloud.com', 'subscribed', '$230956.95', '01/17/2022 07:24 AM']
+['Sandra', 'relievedWasp8@outlook.com', 'unknown', '$140249.2', '01/04/2022 08:23 AM']       
+['Matthew', 'sincereCheetah0@yahoo.com', 'subscribed', '$218325.14', '01/03/2022 10:01 PM']  
+['Alicia', 'madDoughnut9@gmail.com', 'unsubscribed', '$106352.42', '01/05/2022 05:03 PM']    
+['Robert', 'puzzledCardinal3@yahoo.com', 'unsubscribed', '$109170.01', '01/10/2022 04:06 PM']
+'''
+
+```
--- main.py
+++ main.py
@@ -342,34 +342,32 @@ class CSVWriter:
     
 def main():
     # users = UserGenerator().generate(full_names=True, amount=5).getData()
-    # table = DataTable([
-    #     Column('name', NameGenerator(full_name=False, from_list=NameGenerator(25, True).generate().getData())),
-    #     Column('id', IDGenerator()),
-    #     Column('email', EmailGenerator()),
-    #     Column('address', AddressGenerator(with_zips=False)),
-    #     Column('subscription_status', CategoryGenerator(['subscribed', 'unsubscribed', 'unknown'])),
-    #     Column('amount_spent', CurrencyGenerator('$')),
-    #     Column('date', DateGenerator('1/1/2022 1:30 pm', '1/22/2022 1:30 pm')),
-    # ])
     table = DataTable([
-        Column('age', NumberGenerator(num_range=(13,85))),
-        Column('income', CurrencyGenerator('$', amount_range=(0,250000))),
-        Column('product_line', CategoryGenerator(['office_suite'])),
-        Column('prev_purchase', CategoryGenerator(['yes', 'no'])),
-        Column('region', CategoryGenerator(['northeast', 'midwest', 'south', 'west'])),
-        Column('tos(m)', NumberGenerator(num_range=(1,500))),
-        Column('day', CategoryGenerator(['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']))
+        Column('name', NameGenerator(full_name=False)),
+        Column('email', EmailGenerator()),
+        Column('subscription_status', CategoryGenerator(['subscribed', 'unsubscribed', 'unknown'])),
+        Column('amount_spent', CurrencyGenerator('$', amount_range=(0,250000))),
+        Column('date', DateGenerator('1/1/2022 1:30 pm', '1/22/2022 1:30 pm')),
     ])
+    # table = DataTable([
+    #     Column('age', NumberGenerator(num_range=(13,85))),
+    #     Column('income', CurrencyGenerator('$', amount_range=(0,250000))),
+    #     Column('product_line', CategoryGenerator(['office_suite'])),
+    #     Column('prev_purchase', CategoryGenerator(['yes', 'no'])),
+    #     Column('region', CategoryGenerator(['northeast', 'midwest', 'south', 'west'])),
+    #     Column('tos(m)', NumberGenerator(num_range=(1,500))),
+    #     Column('day', CategoryGenerator(['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']))
+    # ])
     
     # defines the number or rows (data points) you'd like
-    table.set_rows(1000)
+    table.set_rows(5)
     # fills the table's the row/coumn data
     table.generate_rows()
     # display the table data in the console
-    # table.print_rows()
+    table.print_rows()
     
     # write table to csv - change name to write to different file
-    CSVWriter.write_to('msft_gen_data.csv', table)
+    # CSVWriter.write_to('msft_gen_data.csv', table)
     return 
 
 main()
